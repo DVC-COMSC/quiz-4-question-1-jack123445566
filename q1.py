4036e5e5d416ef46fefd0fff5759c5e1362b63fa
Jack <jackcshin@gmail.com>
2022-09-30 15:43:17 -0700

    1st commit

---
+++ q1.py
@@ -0,0 +1,9 @@
+length = 10
+count=0
+num_arr=[]
+for i in range(length):
+    num_arr.append(int(input("Enter number "+str(i+1)+ ": ")))
+for i in range(length-1):
+    if num_arr[i]%2==0 and num_arr[i+1]%2==0:
+        count=count+1
+print(count)
