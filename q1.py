af9d9cf74544a6949cd5bde94d3b572f8eb34f06
jack123445566 <112512759+jack123445566@users.noreply.github.com>
2022-09-30 13:57:53 -0700

    Delete q1.py

--- q1.py
+++
@@ -1,9 +0,0 @@
-length=int(input("Enter the length of the sequence: "))
-count=0
-num1=int(input("Enter number "+str(1)+ ": "))
-for i in range(length-1):
-    num2=int(input("Enter number "+str(i+2)+ ": "))
-    if((num1%2==0) and (num2%2==0)):
-        count=count+1
-    num1=num2
-print("The number of pairs of adjacent even numbers is "+ str(count))
